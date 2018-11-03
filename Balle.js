31967bd7b9f9c1adbc20bda7ad834249d552082c
nico12450 <nicolas.gamel12450@gmail.com>
2018-11-03 17:12:54 +0100

    déplacement de la balle sans interactions avec les barres

--- Balle.js
+++ Balle.js
@@ -19,12 +19,17 @@ class Balle {
 		this.position = [0,0]
 		this.vitesse = 10;
 		this.contexte;
+		this.direction = true;
 
+	}
+
+	setPosition(x,y){
+		this.position = [x,y];
 	}
 
 	setContexte(ctx){
 		this.contexte = ctx;
-		this.position = [ctx.canvas.width/2,ctx.canvas.height/2]
+		this.position = [ctx.canvas.width/2-this.taille/2,ctx.canvas.height/2-this.taille/2];
 	}
 
 	draw(){
@@ -36,6 +41,35 @@ class Balle {
 	clear(){
 		this.contexte.fillStyle = 'rgb(200,200,200)';
 		this.contexte.fillRect(this.position[0], this.position[1], this.taille, this.taille);
+
+	}
+
+	move(){
+
+		this.clear();
+
+		this.testRebond();
+
+		if(this.direction){
+			this.setPosition(this.position[0]-this.vitesse,this.position[1]);
+		}
+
+		else{
+			this.setPosition(this.position[0]+this.vitesse,this.position[1]);
+		}
+
+		this.draw();
+	}
+
+	testRebond(){
+
+		if(this.position[0]<0){
+			this.direction = false;
+		}
+
+		else if(this.position[0]>this.contexte.canvas.width){
+			this.direction = true;
+		}
 
 	}
 
